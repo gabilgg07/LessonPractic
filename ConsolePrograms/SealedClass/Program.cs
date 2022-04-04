7f358541610f5d1ae8bbd8a12564c90586669e65
gabilgg07 <gabilgg07@gmail.com>
2022-04-04 06:47:46 +0400

    Lesson 86 Sealed directive

--- ConsolePrograms/SealedClass/Program.cs
+++ ConsolePrograms/SealedClass/Program.cs
@@ -11,4 +11,30 @@ namespace SealedClass
             Console.ReadKey();
         }
     }
+
+
+    sealed class SealedClass
+    {
+        // Sealed class-dan miras almaq olmur.
+        // Bunun proqramimizi, mehsulumuzu qorumaq ucun edirik.
+        // bezen bele mohurlemek lazim olur ki, kimse classimizdan(modelimizden)
+        // miras alib inkisaf etdirib satisa cixarib,
+        // bizim satisimizin qarsisina kecmesin.
+
+        // Kiminse verdiyi .dll file-ni (class library)
+        // oz proqramimizda istifade edende onun yazdigi class-i,
+        // public memberlerini methodlarini goruruk.
+        // Amma hemin methodun icinde yazilanlari gormuruk
+        // ve deyise bilmirik. Sadece istidafe ede bilirik.
+
+        // Miras ala bilirik,
+        // Amma Sealed olarsa miras da ala bilmirik.
+    }
+
+    // Miras almaq olmur.
+
+    //class TringInheritedFromSealedClass: SealedClass
+    //{
+
+    //}
 }
