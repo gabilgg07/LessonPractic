dc9f45d78bb0f7aacb0fcc9aa3476ad026a19c28
gabilgg07 <gabilgg07@gmail.com>
2022-04-07 13:17:53 +0400

    Lesson 92 ended: Static class

--- ConsolePrograms/StaticClassAndMethods/Program.cs
+++ ConsolePrograms/StaticClassAndMethods/Program.cs
@@ -12,6 +12,27 @@ namespace StaticClassAndMethods
 
             AddTeacher(7);
 
+            Console.WriteLine("Next ->");
+            Console.ReadKey();
+
+
+            // Static class-dan her hansi memberini ilk defe cagiranda
+            // instance (constructor)1 defe isleyir.
+            // Sonra butun memberleri o referance-a muraciet edir.
+
+
+            StaticClass.StaticNum = 6;
+
+            Console.ReadKey();
+
+            StaticClass.FirstStaticMethod();
+
+            StaticClass.StaticStr = "test";
+
+            string str = StaticClass.StaticStr;
+
+            StaticClass.SecondStaticMethod(str);
+
             Console.ReadKey();
         }
 
@@ -50,6 +71,23 @@ namespace StaticClassAndMethods
                 Console.WriteLine("Her sey qaydasindadir/ AddStudent");
             }
         }
+
+        // Non-static memberleri ancaq
+        // instance(obyekt) uzerinden cagirmaq olur.
+
+        // Static-leri ise ancaq class adi uzerinden cagirmaq olur.
+
+        // Her defe class-in 1 ve ya 2 methodunu isletmek ucun,
+        // onun instance-ni (nusxesini yaratmaga deymez).
+        // Her nusxe (obyekti) yarananda heap yaddasda
+        // ona  yeni yer ayrilir.
+
+        // Static olan yaddas sahesinde 1 yer tutur,
+        //  instance ise her defesinde yeni yer tutur.
+
+        // Bir class-da non-static member
+        // olmayacaqsa static class etmek olar.
+
     }
 
     class TypeChecker
@@ -94,6 +132,36 @@ namespace StaticClassAndMethods
             {
                 return false;
             }
+        }
+    }
+
+
+    static class StaticClass
+    {
+        // static class icinde yalniz static olan,
+        // parametr qebul etmeyen, private olan ve
+        // yalnizca 1 constructor ola biler.
+
+        static StaticClass()
+        {
+            Console.WriteLine("I am static class.");
+        }
+
+        // static clas icinde ancaq statik memberler ola biler:
+
+        static public int StaticNum;
+
+        static public string StaticStr { get; set; }
+
+        static public void FirstStaticMethod()
+        {
+            Console.WriteLine("I am first statik method in Static Class.");
+        }
+
+
+        static public void SecondStaticMethod(string str)
+        {
+            Console.WriteLine($"I am second statik method in Static Class with: {str}");
         }
     }
 }
