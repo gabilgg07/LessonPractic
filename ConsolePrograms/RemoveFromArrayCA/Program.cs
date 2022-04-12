06a0908665103aef411a3117da8b6977b4f267e6
gabilgg07 <gabilgg07@gmail.com>
2022-04-12 17:37:08 +0400

    Lesson 96 ended: SortedList, Stack, Queue

--- ConsolePrograms/RemoveFromArrayCA/Program.cs
+++ ConsolePrograms/RemoveFromArrayCA/Program.cs
@@ -45,6 +45,7 @@ namespace RemoveFromArrayCA
             ana klasi olan object kimi goturulur.
             Ona gore elementlerinden istifade edende elave olaraq
             boxin-unboxin emeliyyatlarindan da istifade edilir.
+            Generic typelardar boxing-den soz gede bilmez.
 
             --------------------------------------------------------------------
 
@@ -56,52 +57,11 @@ namespace RemoveFromArrayCA
             HashTable.Add(key, value) da ise key ve value teleb edir.
             Yazilan key-e yazilan valune teyin edir.
             key-value pair => key-value birlesmesi.
-            Eyni key-i tekrar add etmek exeption(error) verir.
+            Eyni key-i tekrar Add etmek exeption(error) verir.
 
             ArrayList-de oxuyanda sira nomresine gore oxuyur,
             HashTable-da ise key-e esasen.
              */
-
-            Hashtable hTable = new Hashtable();
-
-            hTable.Add("birinci","on bir");
-            hTable.Add(2,"on iki");
-            hTable.Add("ucuncu",13);
-            hTable.Add(4,14);
-            //hTable.Add(4,3); => eyni key yazmaq olmaz.
-
-            Console.WriteLine(hTable["birinci"]);
-            Console.WriteLine(hTable[2]);
-            Console.WriteLine(hTable["ucuncu"]);
-            Console.WriteLine(hTable[4]);
-
-            Console.WriteLine("-----------------------");
-
-            Hashtable hashtable = new Hashtable();
-
-            string[] profession = new string[] { "analitik", "informasiya muhendisi" };
-
-            hashtable.Add("D6TFH89","Asif Kerimov");
-            hashtable.Add("64TFH8O","Eli Semedov");
-            hashtable.Add("YTF5H39","Sevda Besirzade");
-            hashtable.Add("54TFG89",profession);
-
-            foreach (var key in hashtable.Keys)
-            {
-                object value = hashtable[key];
-
-                if (value is string[] list)
-                {
-                    string joinedArray = string.Join(", ", list);
-
-                    Console.WriteLine($"{key} {joinedArray}");
-                }
-                else
-                {
-                    Console.WriteLine($"{key} {value}");
-                }
-            }
-
 
             Console.ReadKey();
         }
@@ -151,10 +111,11 @@ namespace RemoveFromArrayCA
             Console.WriteLine("==== end ====");
         }
 
-        private static void RemoveFromArrayList()
+        private static void RemoveFromArrayListNonGenericCollection()
         {
             ArrayList list = new ArrayList();
 
+            //  Add(object value);
             list.Add(6);
             list.Add(true);
             list.Add(DateTime.Now);
@@ -181,6 +142,7 @@ namespace RemoveFromArrayCA
                 Console.WriteLine(item);
             }
 
+            // .RemoveAt(index) -> indekse gore silir
             ls.RemoveAt(2);
 
             Console.WriteLine("--- Print Removed 2 index ---");
@@ -190,6 +152,7 @@ namespace RemoveFromArrayCA
                 Console.WriteLine(item);
             }
 
+            // .Clear() -> butun temizleyir.
             ls.Clear();
 
             Console.WriteLine("--- Print Clear ---");
@@ -199,5 +162,156 @@ namespace RemoveFromArrayCA
                 Console.WriteLine(item);
             }
         }
+
+        private static void HashTableNonGenericCollection()
+        {
+            Hashtable hTable = new Hashtable();
+
+            // .Add(object key, object value);
+            hTable.Add("birinci", "on bir");
+            hTable.Add(2, "on iki");
+            hTable.Add("ucuncu", 13);
+            hTable.Add(4, 14);
+            //hTable.Add(4,3); => eyni key yazmaq olmaz.
+
+            // hTable[key] -> key-e esasen value
+            Console.WriteLine(hTable["birinci"]);
+            Console.WriteLine(hTable[2]);
+            Console.WriteLine(hTable["ucuncu"]);
+            Console.WriteLine(hTable[4]);
+
+            Console.WriteLine("-----------------------");
+
+            Hashtable hashtable = new Hashtable();
+
+            string[] profession = new string[] { "analitik", "informasiya muhendisi" };
+
+            hashtable.Add("D6TFH89", "Asif Kerimov");
+            hashtable.Add("64TFH8O", "Eli Semedov");
+            hashtable.Add("YTF5H39", "Sevda Besirzade");
+            hashtable.Add("54TFG89", profession);
+
+            foreach (var key in hashtable.Keys)
+            {
+                object value = hashtable[key];
+
+                if (value is string[] list)
+                {
+                    string joinedArray = string.Join(", ", list);
+
+                    Console.WriteLine($"{key} {joinedArray}");
+                }
+                else
+                {
+                    Console.WriteLine($"{key} {value}");
+                }
+            }
+        }
+
+        private static void SortedListNonGenericCollection()
+        {
+            SortedList sorted = new SortedList();
+
+            // key eyni type olmalidi ki, siralaya bilsin.
+            // string type-i ASCII-ye esasen siralayir.
+            sorted.Add("33", "otuz uc");
+            sorted.Add("55", "elli bes");
+            sorted.Add("44", "qirx dord");
+
+            foreach (var k in sorted.Keys)
+            {
+                object value = sorted[k];
+
+                Console.WriteLine($"{k} {value}");
+            }
+
+            SortedList nums = new SortedList();
+
+            // number type-lari ise kemiyyete gore siralayir
+            nums.Add(4, "dord");
+            nums.Add(1, "bir");
+            nums.Add(32, "otuz iki");
+            nums.Add(13, "on uc");
+
+            foreach (var k in nums.Keys)
+            {
+                Console.WriteLine($"{k} -> {nums[k]}");
+            }
+        }
+
+        private static void StackAndQueueNonGenericCollections()
+        {
+            Console.WriteLine("----- Stack ----");
+
+            // Stack - yigin, deste (torba, qutu mentiqi)
+            // Lifo - last      in     first    out
+            //        sonuncu   giren  birinci  cixar
+
+            Stack stc = new Stack();
+
+            // push - itelemek (torbaya itelemek)
+
+            stc.Push(7);
+            stc.Push(67);
+            stc.Push(4);
+            stc.Push(56);
+
+            // pop - cixartmaq sonuncu qoyulani.
+            object x1 = stc.Pop();
+            Console.WriteLine(x1); // 56 sonuncu, 1-ci cixdi.
+
+            object x2 = stc.Pop();
+            Console.WriteLine(x2); // 4
+
+            object x3 = stc.Pop();
+            Console.WriteLine(x3); // 67
+
+            Console.WriteLine("-----------------------");
+
+            stc.Push(x2);
+            stc.Push(x1);
+
+            foreach (var x in stc)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine();
+
+            // Queue[kyu:] - sira, novbe (boru mentiqi)
+            // Fifo - first  in     first  out
+            //        1-ci   giren  1-ci   cixr
+            Console.WriteLine("----- Queue ----");
+
+            Queue queue = new Queue();
+
+            // Enqueue - novbeye salmaq
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+
+            Console.WriteLine("Firs queue:");
+
+            foreach (var y in queue)
+            {
+                Console.WriteLine(y);
+            }
+
+            Console.WriteLine("Dequeued elements:");
+
+            // Dequeue - siradan, novbeden cixartmaq
+            object y1 = queue.Dequeue();
+            Console.WriteLine(y1);
+
+            object y2 = queue.Dequeue();
+            Console.WriteLine(y2);
+
+            object y3 = queue.Dequeue();
+            Console.WriteLine(y3);
+
+        }
+
+
     }
 }
