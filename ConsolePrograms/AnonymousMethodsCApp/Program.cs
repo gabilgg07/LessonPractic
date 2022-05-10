1a1266479c32b086ed5a086a70d20d10e462bdd3
gabilgg07 <gabilgg07@gmail.com>
2022-05-10 12:20:59 +0400

    Callback methods, Lambda methods

--- ConsolePrograms/AnonymousMethodsCApp/Program.cs
+++ ConsolePrograms/AnonymousMethodsCApp/Program.cs
@@ -6,7 +6,7 @@ namespace AnonymousMethodsCApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("MethodWithName() =>");
+            Console.WriteLine("MethodWithName() ->");
             // adli method
             MethodWithName();
 
@@ -14,10 +14,10 @@ namespace AnonymousMethodsCApp
             Action action = MethodWithName;
 
 
-            Console.WriteLine("action() =>");
+            Console.WriteLine("action() ->");
             action();
 
-            Console.WriteLine("action.Invoke() =>");
+            Console.WriteLine("action.Invoke() ->");
             action.Invoke();
 
             Action withAnonymousMethod = delegate
@@ -26,11 +26,11 @@ namespace AnonymousMethodsCApp
                 Console.WriteLine("Called anonymous method!\n");
             };
 
-            Console.WriteLine("withAnonymousMethod() =>");
+            Console.WriteLine("withAnonymousMethod() ->");
             withAnonymousMethod();
 
 
-            Console.WriteLine("RunSafe(anonymous method) =>");
+            Console.WriteLine("RunSafe(delegate{ ... }) ->");
             // eyni codu tekrar yaziriqsa, bu methodu anonym yox adli method kimi istifade etmeliyik
             // anonym methodlari 1 defe istifade edilecekse yazilir.
             RunSafe(delegate
@@ -38,9 +38,37 @@ namespace AnonymousMethodsCApp
                 Console.WriteLine("Called anonymous method!\n");
             });
 
+            // Anonymous method with parametr:
+            Action<string> anonymousMethodWithParametr = delegate (string str)
+            {
+                Console.WriteLine($"Called Anonymous method with parametr, value is: {str}!\n");
+            };
+
+            Console.WriteLine("Action<string> anonymousMethodWithParametr = delegate(string str){ ... } -> \n" +
+                "anonymousMethodWithParametr(\"AnonymousMethodValue\") -> ");
+            anonymousMethodWithParametr("AnonymousMethodValue");
+
+            // Lambda method:
+            Console.WriteLine("RunSafe(() => { ... })->");
+            RunSafe(() =>
+            {
+                Console.WriteLine("Called Lambda method!\n");
+            });
+
+            // Lambda method with parametr:
+            Action<int> lambdaMethodWithParametr = (int x) =>
+            {
+                Console.WriteLine($"Called Lambda method with parametr, value is: {x}!\n");
+            };
+
+            Console.WriteLine("Action<int> lambdaMethodWithParametr = (int x) => { ... } -> \n" +
+                "ambdaMethodWithParametr(12) ->");
+            lambdaMethodWithParametr(12);
+
             Console.ReadKey();
         }
-
+        // Callback methods:
+        // Method parametr kimi gonderilir ve body-de istifade edilir:
         private static void RunSafe(Action action)
         {
             try
